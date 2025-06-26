5b450ecfb8552ad4657b80531bfb1a44ba3f0953
Christopher Unsell <cdojrf@gmail.com>
2025-06-26 02:12:58 -0600

    static_root in settings

--- c868/settings.py
+++ c868/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
