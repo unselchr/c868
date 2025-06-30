9ca4a0c0ac29ed6a22aaf20e9accb0f990b38eb0
Christopher Unsell <cdojrf@gmail.com>
2025-06-30 15:28:22 -0500

    remove app_dirs

--- c868/settings.py
+++ c868/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR.parent, 'templates'), 'templates'],
-        'APP_DIRS': True,
+        # 'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
